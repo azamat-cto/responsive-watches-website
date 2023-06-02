80554d4f994e5cc69257ec86ed78bf87eef2ee70
azamatkha6i6ullaev <azamatkha6i6ullaev@gmail.com>
2023-06-03 03:00:23 +0500

    Add react multi carousel for news section

--- src/components/New/New.tsx
+++ src/components/New/New.tsx
@@ -1,6 +1,25 @@
 import { Container } from '@mui/material'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import { items } from '../../constants'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+}
 
 export const New = () => {
   return (
@@ -8,25 +27,23 @@ export const New = () => {
       <Container>
         <h2 className="section__title">New Arrivals</h2>
 
-        <div className="new__grid">
-          <Swiper spaceBetween={24} loop={true}>
+        <div className="new__grid grid">
+          <Carousel responsive={responsive}>
             {items.map((item) => (
-              <SwiperSlide key={item.key}>
-                <article className="new__card swiper-slide">
-                  <span className="new__tag">New</span>
+              <article className="new__card swiper-slide" key={item.key}>
+                <span className="new__tag">New</span>
 
-                  <img className="new__img" src={item.img} alt={item.title} />
+                <img className="new__img" src={item.img} alt={item.title} />
 
-                  <div className="new__data">
-                    <h3 className="new__title">{item.title}</h3>
-                    <span className="new__price">{item.price}</span>
-                  </div>
+                <div className="new__data">
+                  <h3 className="new__title">{item.title}</h3>
+                  <span className="new__price">{item.price}</span>
+                </div>
 
-                  <button className="button new__button">ADD TO CART</button>
-                </article>
-              </SwiperSlide>
+                <button className="button new__button">ADD TO CART</button>
+              </article>
             ))}
-          </Swiper>
+          </Carousel>
         </div>
       </Container>
     </section>
